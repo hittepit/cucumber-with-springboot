3c8e85c34c94c8b60bc1d6c3424263e0ce194b2e
fabrice.claes <fabrice.claes@ucm.be>
2024-06-10 11:56:26 +0200

    Test fonctionnel (mais pas de rollback?)

--- src/main/java/be/ucm/pocs/springboot/cucumber/rest/EmployeurController.java
+++ src/main/java/be/ucm/pocs/springboot/cucumber/rest/EmployeurController.java
@@ -3,6 +3,7 @@ package be.ucm.pocs.springboot.cucumber.rest;
 import be.ucm.pocs.springboot.cucumber.services.EmployeurService;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +17,7 @@ public class EmployeurController {
     }
 
     @PostMapping("/{numDos}")
-    public int create(@PathVariable("numDos") String numeroDossier, String denomination) {
+    public int create(@PathVariable("numDos") String numeroDossier, @RequestBody String denomination) {
         return employeurService.create(numeroDossier, denomination);
     }
 }
--- src/test/java/be/ucm/pocs/springboot/cucumber/steps/EmployeurSteps.java
+++ src/test/java/be/ucm/pocs/springboot/cucumber/steps/EmployeurSteps.java
@@ -1,12 +1,32 @@
 package be.ucm.pocs.springboot.cucumber.steps;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import be.ucm.pocs.springboot.cucumber.dao.EmployeurRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.jupiter.api.Assertions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 public class EmployeurSteps {
+    private final MockMvc mockMvc;
+    private final ObjectMapper objectMapper;
+    private final EmployeurRepository employeurRepository;
+
     private String numeroDossier;
     private String denomination;
+
+    public EmployeurSteps(MockMvc mockMvc, ObjectMapper objectMapper, EmployeurRepository employeurRepository) {
+        this.mockMvc = mockMvc;
+        this.objectMapper = objectMapper;
+        this.employeurRepository = employeurRepository;
+    }
 
     @Given("{string} as numero dossier")
     public void asNumeroDossier(String numeroDossier) {
@@ -19,11 +39,14 @@ public class EmployeurSteps {
     }
 
     @When("I create an employeur")
-    public void iCreateAnEmployeur() {
-
+    public void iCreateAnEmployeur() throws Exception {
+        Assertions.assertNotNull(mockMvc);
+        mockMvc.perform(post("/api/employeur/"+numeroDossier).content(objectMapper.writeValueAsString(denomination)))
+                .andExpect(status().isOk());
     }
 
     @Then("a new employeur is created with {string} as numero dossier and {string} as denomination")
     public void aNewEmployeurIsCreatedWithAsNumeroDossierAndAsDenomination(String numeroDossier, String denomination) {
+        assertEquals(1,employeurRepository.findAll().size());
     }
 }
